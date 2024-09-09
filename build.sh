025076b4bb343af371355f35dfe41f9fdeebf615
Ahmed Almadhoob <me@amstalks.com>
2024-09-09 23:56:07 +0300

    Create a build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd controller/push-swap/
+go build main.go
+chmod u+x main
+mv main ../../push-swap
+cd ../../
+cd controller/checker/
+go build main.go
+chmod u+x main
+mv main ../../checker
+cd ../../
